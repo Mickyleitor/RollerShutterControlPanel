cd00d4453344f4b59b77e005ba710bdba14c643a
Mickyleitor <micky@uma.es>
2019-09-08 00:38:32 +0200

    Lcd messages

--- DomoticShutter.ino
+++ DomoticShutter.ino
@@ -6,6 +6,12 @@
 LiquidCrystal_PCF8574 lcd(ADDRESS_I2C_LCD);
 
 int task_button = -1;
+int selected_shutter = 1;
+enum state {GOING_DOWN,STOPPED,GOING_UP};
+String shuttername [3] = {"Persiana derecha","Persiana central","Persiana lateral"};
+state shutterstate [3] = {STOPPED,STOPPED,STOPPED};
+int shutterprevioustime [3] = {0,0,0};
+int shuttercurrenttime [3] = {0,0,0}; 
 
 void setup(){
   Serial.begin(9600);
@@ -17,7 +23,10 @@ void setup(){
   
   lcd.begin(16,2);
   lcd.setBacklight(255);
-  lcd.home(); lcd.clear();
+  byte customArrayChar[5][8] = {{0x00,0x00,0x1F,0x1F,0x0E,0x04,0x00,0x00},{0x00,0x00,0x04,0x0E,0x1F,0x1F,0x00,0x00},{0x00,0x00,0x06,0x0E,0x1E,0x0E,0x06,0x00},{0x00,0x00,0x0C,0x0E,0x0F,0x0E,0x0C,0x00},{0x00,0x0E,0x1F,0x11,0x11,0x1F,0x0E,0x00}};
+  for ( int i = 0 ; i < 5 ; i ++ ) lcd.createChar(i, customArrayChar[i]);
+
+  updateScreen();
  
   DDRB &= ~bit(DDB0) | ~bit(DDB1) | ~bit(DDB2) | ~bit(DDB3); // Clear the PBX pin
   // PBX (PCINT0-5 pin) is now an input
@@ -29,26 +38,60 @@ void setup(){
 void loop(){
     switch (task_button) {
       case PIN_BUTTON_LEFT : {
-        if(!sendCommand(2,0)) beep_alarm(BUZZER_MEDIUM_VOLUME,100,3);
+        selected_shutter = (selected_shutter+1) % 3;
+        updateScreen();
         task_button = -1;
         break;
       }
+
+      // Not sure how to handle this following two buttons, they behave exactly in almost the same way
       case PIN_BUTTON_CANCEL : {
-        if(!sendCommand(2,1)) beep_alarm(BUZZER_MEDIUM_VOLUME,100,3);
+        if(shutterstate[selected_shutter] == GOING_UP){
+          if(sendCommand(selected_shutter,STOPPED)){
+            shuttercurrenttime[selected_shutter] -= (millis() - shutterprevioustime[selected_shutter]);
+            shutterstate[selected_shutter] = STOPPED;
+          }
+        }else{
+          if(sendCommand(selected_shutter,GOING_UP)){
+            if(shutterstate[selected_shutter] == GOING_DOWN){
+              shuttercurrenttime[selected_shutter] += (millis() - shutterprevioustime[selected_shutter]);
+            }
+            shutterprevioustime[selected_shutter] = millis();
+            shutterstate[selected_shutter] = GOING_UP;
+          }
+        }
+        shuttercurrenttime[selected_shutter] = constrain(shuttercurrenttime[selected_shutter], 0, SHUTTER_CICLETIME);
+        updateScreen();
         task_button = -1;
         break;
       }
       case PIN_BUTTON_OK : {
-        if(!sendCommand(2,2)) beep_alarm(BUZZER_MEDIUM_VOLUME,100,3);
+        if(shutterstate[selected_shutter] == GOING_DOWN){
+          if(sendCommand(selected_shutter,STOPPED)){
+            shuttercurrenttime[selected_shutter] += (millis() - shutterprevioustime[selected_shutter]);
+            shutterstate[selected_shutter] = STOPPED;
+          }
+        }else{
+          if(sendCommand(selected_shutter,GOING_DOWN)){
+            if(shutterstate[selected_shutter] == GOING_UP){
+              shuttercurrenttime[selected_shutter] -= (millis() - shutterprevioustime[selected_shutter]);
+            }
+            shutterprevioustime[selected_shutter] = millis();
+            shutterstate[selected_shutter] = GOING_DOWN;
+          }
+        }
+        shuttercurrenttime[selected_shutter] = constrain(shuttercurrenttime[selected_shutter], 0, SHUTTER_CICLETIME);
+        updateScreen();
         task_button = -1;
         break;
       }
       case PIN_BUTTON_RIGHT : {
-        if(!sendCommand(1,1)) beep_alarm(BUZZER_MEDIUM_VOLUME,100,3);
+        selected_shutter = (selected_shutter < 1) ? 2 : selected_shutter - 1;
+        updateScreen();
         task_button = -1;
         break;
       }
-    }    
+    }
 }
 
 // This shouldn't be used inside ISR
@@ -99,16 +142,22 @@ ISR (PCINT0_vect)
   }
 }
 
+
+// Send a RF 'order' to 'shutter'
 bool sendCommand(unsigned int shutter, unsigned int order){
   Tcommand command;
-  if(shutter < 3 && order < 3){
+  if(shutter < 3 && order < 3){ // Check out of range
     EEPROM.get( ((shutter * 3) + order) * sizeof(struct Tcommand), command);
-    if(CRC8(command.Key,KEY_LENGTH) == command.CRC){
-      int bits = 0;
-      for(int index = 0; index < KEY_LENGTH ; index++){
-        for(bits = 7; bits >= 0; bits--){
-          bitWrite(PORT_RF_TX,PIN_RF_TX,(command.Key[index] >> bits) & 1);
-          delayMicroseconds(490);
+    if(CRC8(command.Key,KEY_LENGTH) == command.CRC){ // Check true data
+      for(int i = 0 ; i < REPEAT_MSG_TIMES ; i ++){
+        // Loop bit per bit the data writting the bit state 0-1 to the TX pin
+        int bits = 0;
+        for(int index = 0; index < KEY_LENGTH ; index++){
+          for(bits = 7; bits >= 0; bits--){
+            bitWrite(PORT_RF_TX,PIN_RF_TX,(command.Key[index] >> bits) & 1);
+            // Delay 460 uS every bit
+            delayMicroseconds(490);
+          }
         }
       }
       return true;
@@ -118,4 +167,30 @@ bool sendCommand(unsigned int shutter, unsigned int order){
   }else{
     return false;
   }
+}
+
+void updateScreen(){
+  lcd.home(); lcd.clear();
+  lcd.print(shuttername[selected_shutter]);
+  lcd.setCursor(1,1);
+  lcd.write(LCD_CHAR_ARROWLEFT);
+  lcd.setCursor(5,1);
+  if(shutterstate[selected_shutter] == GOING_UP){
+    lcd.write(LCD_CHAR_STOP);
+  }else{
+    lcd.write(LCD_CHAR_ARROWUP);
+  }
+  /*
+  lcd.setCursor(7,1);
+  lcd.print(shuttercurrenttime[selected_shutter]/1000);
+  */
+  lcd.setCursor(10,1);
+  if(shutterstate[selected_shutter] == GOING_DOWN){
+    lcd.write(LCD_CHAR_STOP);
+  }else{
+    lcd.write(LCD_CHAR_ARROWDOWN);
+  }
+  lcd.setCursor(14,1);
+  lcd.write(LCD_CHAR_ARROWRIGHT);
+
 }
--- basic_defines.h
+++ basic_defines.h
@@ -14,8 +14,16 @@
 #define KEY_LENGTH 92
 #define KEY_CRC_LENGTH 1
 #define DEBUGGING 1
+#define REPEAT_MSG_TIMES 2
+#define SHUTTER_CICLETIME 20000
 
 #define ADDRESS_I2C_LCD 0x3F
+#define LCD_CHAR_ARROWDOWN byte(0)
+#define LCD_CHAR_ARROWUP byte(1)
+#define LCD_CHAR_ARROWLEFT byte(2)
+#define LCD_CHAR_ARROWRIGHT byte(3)
+#define LCD_CHAR_STOP byte(4)
+
 
 struct Tcommand {
   byte Key [KEY_LENGTH];

99834f88390338d6630c062f1dc069cbd30e97f7
Mickyleitor <micky@uma.es>
2020-04-07 14:03:10 +0200

    Introduced timeLib and New requirements

--- Firmware_Arduino/Firmware_Arduino.ino
+++ Firmware_Arduino/Firmware_Arduino.ino
@@ -15,14 +15,15 @@ int shutterprevioustime [3] = {0,0,0};
 int shuttercurrenttime [3] = {0,0,0};
 unsigned long LastTimeButtonPressed = 0xFFFF;
 unsigned long LastTimeScreenRefreshed = 0xFFFF;
-time_t T;
 
-void setup(){
+void setup(){ 
+  // delay(5000);
   Serial.begin(9600);
   do{
     delay(1000);
     Wire.begin();
     Wire.beginTransmission(ADDRESS_I2C_LCD);
+    tone(PIN_BUZZER,BUZZER_HIGH_VOLUME,500);
   }while(Wire.endTransmission() != 0);
   
   lcd.begin(16,2);
@@ -30,15 +31,13 @@ void setup(){
   byte customArrayChar[5][8] = {{0x00,0x00,0x1F,0x1F,0x0E,0x04,0x00,0x00},{0x00,0x00,0x04,0x0E,0x1F,0x1F,0x00,0x00},{0x00,0x00,0x06,0x0E,0x1E,0x0E,0x06,0x00},{0x00,0x00,0x0C,0x0E,0x0F,0x0E,0x0C,0x00},{0x00,0x0E,0x1F,0x11,0x11,0x1F,0x0E,0x00}};
   for ( int i = 0 ; i < 5 ; i ++ ) lcd.createChar(i, customArrayChar[i]);
 
-  setTime(14,33,30,9,9,2019); // 12:00 (H24) 9 de Septiembre del 2019
-  
-  updateMainScreen();
- 
   DDRB &= ~bit(DDB0) | ~bit(DDB1) | ~bit(DDB2) | ~bit(DDB3); // Clear the PBX pin
   // PBX (PCINT0-5 pin) is now an input
   PCMSK0 |= bit (PCINT0) | bit (PCINT1) |bit (PCINT2) |bit (PCINT3); // set PCINT0-3 to trigger an interrupt on state change 
   PCIFR  |= bit (PCIF0);   // clear any outstanding interrupts
   PCICR |= bit (PCIF0); // set PCIE0 to enable PCMSK0 scan
+  
+  setTime(21,31,00,4,4,2020); // 12:00 (H24) 9 de Septiembre del 2019
 }
 
 void loop(){
@@ -50,13 +49,11 @@ void loop(){
         break;
       }
       case PIN_BUTTON_CANCEL : {
-        moveShutter(GOING_UP);
         updateScreen();
         task_button = TASK_UPDATE_LCD;
         break;
       }
       case PIN_BUTTON_OK : {
-        moveShutter(GOING_DOWN);
         updateScreen();
         task_button = TASK_UPDATE_LCD;
         
@@ -70,38 +67,11 @@ void loop(){
       }
       case TASK_UPDATE_LCD : {
         if ((millis() - LastTimeButtonPressed > SHUTTER_CICLETIME) && (millis() - LastTimeScreenRefreshed > 1000))  {
+          LastTimeButtonPressed
           updateMainScreen();
         }
       }
     }
-}
-
-// This shouldn't be used inside ISR
-void beep_alarm(unsigned int volume, unsigned long period_ms, unsigned int times){
-  for(int i = 0; i < times; i ++){
-    tone(PIN_BUZZER,volume,period_ms);
-    delay(2*period_ms);
-  }
-}
-
-byte CRC8(const byte *data, size_t dataLength)
-{
-  byte crc = 0x00;
-  while (dataLength--) 
-  {
-    byte extract = *data++;
-    for (byte tempI = 8; tempI; tempI--)
-   {
-      byte sum = (crc ^ extract) & 0x01;
-      crc >>= 1;
-      if (sum)
-     {
-        crc ^= 0x8C;
-      }
-      extract >>= 1;
-    }
-  }
-  return crc;
 }
 
 ISR (PCINT0_vect)
@@ -123,33 +93,6 @@ ISR (PCINT0_vect)
     task_button = PIN_BUTTON_RIGHT;
   }
   LastTimeButtonPressed = millis();
-}
-
-
-// Send a RF 'order' to 'shutter'
-bool sendCommand(unsigned int shutter, unsigned int order){
-  Tcommand command;
-  if(shutter < 3 && order < 3){ // Check out of range
-    EEPROM.get( ((shutter * 3) + order) * sizeof(struct Tcommand), command);
-    if(CRC8(command.Key,KEY_LENGTH) == command.CRC){ // Check true data
-      for(int i = 0 ; i < REPEAT_MSG_TIMES ; i ++){
-        // Loop bit per bit the data writting the bit state 0-1 to the TX pin
-        int bits = 0;
-        for(int index = 0; index < KEY_LENGTH ; index++){
-          for(bits = 7; bits >= 0; bits--){
-            bitWrite(PORT_RF_TX,PIN_RF_TX,(command.Key[index] >> bits) & 1);
-            // Delay 460 uS every bit
-            delayMicroseconds(490);
-          }
-        }
-      }
-      return true;
-    }else{
-      return false;
-    }
-  }else{
-    return false;
-  }
 }
 
 void updateScreen(){
@@ -181,28 +124,7 @@ void updateScreen(){
 }
 
 void updateMainScreen(){
-  /*
-  lcd.home(); lcd.clear();
-  
-
-  lcd.setCursor(6,0);
-  lcd.print(hour(T));  
-  lcd.print(+ ":") ;
-  lcd.print(minute(T));
-  // lcd.print(+ ":") ;
-  // lcd.print(second(T));
-  lcd.setCursor(4,1);
-  lcd.print(day(T));
-  lcd.print(+ "/") ;
-  lcd.print(month(T));
-  lcd.print(+ "/") ;
-  lcd.print(year(T)); 
-  lcd.print( " ") ;
-
-  LastTimeScreenRefreshed = millis();
-  lcd.setBacklight(0);
-  */
-  T = now();
+  time_t T = now();
   lcd.clear();
   lcd.setCursor(0, 1);
   if (day(T)<10) lcd.print('0');
@@ -255,22 +177,4 @@ void updateMainScreen(){
   }
   LastTimeScreenRefreshed = millis();
   lcd.setBacklight(0);
-}
-
-void moveShutter(state GOING_TO){
-  if(shutterstate[selected_shutter] == GOING_TO){
-    if(sendCommand(selected_shutter,STOPPED)){
-      shuttercurrenttime[selected_shutter] += ((GOING_TO == GOING_UP) ? -1 : 1)*(millis() - shutterprevioustime[selected_shutter]);
-      shutterstate[selected_shutter] = STOPPED;
-    }
-  }else{
-    if(sendCommand(selected_shutter,GOING_TO)){
-      if(shutterstate[selected_shutter] == ((GOING_TO == GOING_UP) ? GOING_DOWN : GOING_UP)){
-        shuttercurrenttime[selected_shutter] += ((GOING_TO == GOING_UP) ? 1 : -1)*(millis() - shutterprevioustime[selected_shutter]);
-      }
-      shutterprevioustime[selected_shutter] = millis();
-      shutterstate[selected_shutter] = GOING_TO;
-    }
-  }
-  shuttercurrenttime[selected_shutter] = constrain(shuttercurrenttime[selected_shutter], 0, SHUTTER_CICLETIME);
 }
--- Firmware_Arduino/basic_defines.h
+++ Firmware_Arduino/basic_defines.h
@@ -1,3 +1,4 @@
+// Macros for PIN and PORTs
 #define PIN_BUZZER 5
 #define PIN_RF_TX 3
 #define PORT_RF_TX PORTD
@@ -7,27 +8,28 @@
 #define PIN_BUTTON_CANCEL 1
 #define PIN_BUTTON_OK 2
 #define PIN_BUTTON_RIGHT 3
+
+// Macros for buzzer volume selections
 #define BUZZER_LOW_VOLUME 24464
 #define BUZZER_MEDIUM_VOLUME 300
 #define BUZZER_HIGH_VOLUME 5000
 
+// Macros for Task handling
+#define TASK_BUTTON_LEFT 0
+#define TASK_BUTTON_CANCEL 1
+#define TASK_BUTTON_OK 2
+#define TASK_BUTTON_RIGHT 3
 #define TASK_UPDATE_LCD 4
 
-#define KEY_LENGTH 92
-#define KEY_CRC_LENGTH 1
+// Misc
 #define DEBUGGING 1
 #define REPEAT_MSG_TIMES 2
 #define SHUTTER_CICLETIME 20000
 
+// Macros for LCD handing
 #define ADDRESS_I2C_LCD 0x3F
 #define LCD_CHAR_ARROWDOWN byte(0)
 #define LCD_CHAR_ARROWUP byte(1)
 #define LCD_CHAR_ARROWLEFT byte(2)
 #define LCD_CHAR_ARROWRIGHT byte(3)
 #define LCD_CHAR_STOP byte(4)
-
-
-struct Tcommand {
-  byte Key [KEY_LENGTH];
-  byte CRC;
-};

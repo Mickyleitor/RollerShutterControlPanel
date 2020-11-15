e2b83d0c05f26910aadda9e148a18aeeb145aa67
Mickyleitor <micky@uma.es>
2020-11-16 00:11:56 +0100

    Added criticalData only tfg branch
    Pushed criticalData.h to allow the compilation by the jury reviewing this project

---
+++ Firmware_ESP8266/CriticalData.h
@@ -0,0 +1,5 @@
+#define MySSID "CHANGE_THIS"
+#define MyPassword "CHANGE_ALSO_THIS"
+#define MyHTTPrequest "GET /data/2.5/weather?lat=36.7149969&lon=-4.4776851&appid=AND_ALSO_CHANGE_THIS_API_KEY HTTP/1.0"
+#define MY_LAT 36.7149969
+#define MY_LON -4.4776851
